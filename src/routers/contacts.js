a9d6699a63e74ef6d9fe2866683393459d305550
Irina_Piskokha <4526937@gmail.com>
2025-01-31 16:05:03 +0200

    add validation

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,17 +7,36 @@ import {
   patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+import { isValid } from '../middlewares/isValidId.js';
 
 const router = Router();
 
 router.get('/contacts', getContactsController);
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get(
+  '/contacts/:contactId',
+  isValid,
+  ctrlWrapper(getContactByIdController),
+);
 
-router.post('/contacts', createContactController);
+router.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
 
-router.patch('/contacts/:contactId', patchContactController);
+router.patch(
+  '/contacts/:contactId',
+  validateBody(updateContactSchema),
+  isValid,
+  ctrlWrapper(patchContactController),
+);
 
-router.delete('/contacts/:contactId', deleteContactController);
+router.delete('/contacts/:contactId', isValid, deleteContactController);
 
 export default router;
