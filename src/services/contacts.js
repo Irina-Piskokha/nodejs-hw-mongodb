045ee2c292c590523927f5ecc63ed9662b30f322
Irina_Piskokha <4526937@gmail.com>
2025-02-13 12:44:59 +0200

    add _id: contactId

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -39,7 +39,7 @@ export const getAllContacts = async ({
 };
 
 export const getContactById = async (contactId, userId) => {
-  const contact = await ContactsCollection.findOne({ contactId, userId });
+  const contact = await ContactsCollection.findOne({ _id: contactId, userId });
   return contact;
 };
 
