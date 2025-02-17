04313a1e1858143ef4cc8cb7e5285c6de0c70de1
Irina_Piskokha <4526937@gmail.com>
2025-02-17 15:06:38 +0200

    add reset password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,6 +5,7 @@ import {
   refreshUsersSession,
 } from '../services/auth.js';
 import { ONE_DAY } from '../constants/index.js';
+import { requestResetToken, resetPassword } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
   const user = await registerUser(req.body);
@@ -30,7 +31,7 @@ export const loginUserController = async (req, res) => {
 
   res.json({
     status: 200,
-    message: 'Successfuly logged in an user!',
+    message: 'Successfully logged in an user!',
     data: {
       accessToken: session.accessToken,
     },
@@ -72,5 +73,24 @@ export const refreshUserSessionController = async (req, res) => {
     data: {
       accessToken: session.accessToken,
     },
+  });
+};
+
+export const requestResetEmailController = async (req, res) => {
+  await requestResetToken(req.body.email);
+
+  res.json({
+    message: 'Reset password email was successfully sent!',
+    status: 200,
+    data: {},
+  });
+};
+
+export const resetPasswordController = async (req, res) => {
+  await resetPassword(req.body);
+  res.json({
+    message: 'Password was successfully reset!',
+    status: 200,
+    data: {},
   });
 };
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -8,6 +8,14 @@ import {
   refreshUserSessionController,
 } from '../controllers/auth.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import {
+  requestResetEmailSchema,
+  resetPasswordSchema,
+} from '../validation/auth.js';
+import {
+  requestResetEmailController,
+  resetPasswordController,
+} from '../controllers/auth.js';
 
 const router = Router();
 
@@ -26,4 +34,17 @@ router.post(
 router.post('/logout', ctrlWrapper(logoutUserController));
 
 router.post('/refresh', ctrlWrapper(refreshUserSessionController));
+
+router.post(
+  '/send-reset-email',
+  validateBody(requestResetEmailSchema),
+  ctrlWrapper(requestResetEmailController),
+);
+
+router.post(
+  '/reset-password',
+  validateBody(resetPasswordSchema),
+  ctrlWrapper(resetPasswordController),
+);
+
 export default router;
